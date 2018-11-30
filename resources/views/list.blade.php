93fbae9eab2b1afe03520f94a56f2f457138ecc0
acethamarai <acethamarai@gmail.com>
2018-11-30 09:35:21 +0530

    db update

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,44 +1,5 @@
 @extends('app')
 @section('content')
-<!-- Styles -->
-<style>
-	html, body {
-		background-color: #fff;
-		color: #636b6f;
-		font-family: 'Nunito', sans-serif;
-		font-weight: 200;
-		height: 100vh;
-		margin: 0;
-	}
-	.book-price {
-		position: absolute;
-		left: 10px;
-		bottom: 10px;
-	}
-	.book-author {
-		position: absolute;
-		right: 10px;
-		bottom: 10px;
-	}
-	.book-item {
-		position: relative;float:left;width:18%;margin:0 1%; margin-bottom:20px;min-width:164px;height: 410px;border:1px solid;
-	}
-	.book-image{width:100%;}
-	.book-image a{width:100%;height:100%;display:block;}
-	.book-image img{max-width:100%;}
-	
-	.book-title {
-		    line-height: 20px;font-size:18px;text-align:center;
-			padding: 5px;
-    color: black;
-    font-weight: 600;
-	}
-	.bdescription > img{width: 200px; float: left; padding: 10px;}
-	.bprice{color:green;}
-	.bauthor{font-style:italic;color:#aaa;}
-	.ajax-loader img{width:76px;}
-   
-</style>
     </head>
     <body>
         <div class="row">
@@ -77,6 +38,22 @@ $(document).ready(function(){
 	// Inital Load data
 	getLoadData();
 	windowOnScroll();	
+	
+	// Single Book details
+	$(window).on('hashchange', function(e){
+    var origEvent = e.originalEvent;
+    //console.log('Going to: ' + origEvent.newURL + ' from: ' + origEvent.oldURL);
+	var hashstr = window.location.hash.substr(1);
+	if( hashstr == "" || hashstr == "NULL" ){ 
+	$('.books-wall').css('display','block'); 
+	$('#pageContent').css('display','none');
+	//getLoadData(); 
+	} else { 
+	$('.books-wall').css('display','none');
+	$('#pageContent').css('display','block');
+	checkURL(hashstr); 
+	}
+	});
 });
 var imageUrl = "{{ asset('images/books/') }}";
 function windowOnScroll() {
@@ -103,6 +80,7 @@ function getMoreData(lastId) {
         	   setTimeout(function() {
                 $('.ajax-loader').hide();
 		   $('#total_count').val(data.datacounts);
+		   // Render books list
 			$.each(data.books, function (index, books) {
                 $('#books').append('<div class="book-item" id="'+ books.id +'" ><div class="book-image"><a href="#'+ books.slug +'"><image src="'+imageUrl + '/' + books.image+'" alt ="'+books.title+'" ></a></div><div class="book-title" onclick="showBook('+ books.id +')">' + books.title + '</div><div class="book-price">$' + books.price + '</div><div class="book-author">' + books.name + '</div></div>');
             });
@@ -124,6 +102,7 @@ function getLoadData() {
                 $('.ajax-loader').hide();
 			//var imageUrl = "{{ asset('images/books/') }}";
 			$('#total_count').val(data.datacounts);
+			// Render books list
             $.each(data.books, function (index, books) {
                 $('#books').append('<div class="book-item" id="'+ books.id +'" ><div class="book-image"><a href="#'+ books.slug +'"><image src="'+imageUrl + '/' + books.image+'" alt ="'+books.title+'" ></a></div><div class="book-title" onclick="showBook('+ books.id +')">' + books.title + '</div><div class="book-price">$' + books.price + '</div><div class="book-author">' + books.name + '</div></div>');
             });
@@ -134,7 +113,6 @@ function getLoadData() {
 
 //Show single book information as POPUP - METHOD -1
 function showBook(id) {	
-	
 	$.ajax({
 	type: "POST",
 	url: "{{ url('/') }}/api/bookid",
@@ -173,7 +151,7 @@ function loadPage(url)  //the function that loads pages via AJAX
 {
     url = url.replace('#','');   
 	
-    $('.ajax-loader').css('visibility','visible');  //show the rotating gif animation
+    $('.ajax-loader').css('visibility','visible'); 
 
     $.ajax({ 
         type: "GET",
@@ -192,24 +170,6 @@ function loadPage(url)  //the function that loads pages via AJAX
     });
 
 }
-$(document).ready(function(){
-	$(document).on('click', '.book-image a', function(){
-		//checkURL(this.hash); 
-	});
-	$(window).on('hashchange', function(e){
-    var origEvent = e.originalEvent;
-    //console.log('Going to: ' + origEvent.newURL + ' from: ' + origEvent.oldURL);
-	var hashstr = window.location.hash.substr(1);
-	if( hashstr == "" || hashstr == "NULL" ){ 
-	$('.books-wall').css('display','block'); 
-	$('#pageContent').css('display','none');
-	//getLoadData(); 
-	} else { 
-	$('.books-wall').css('display','none');
-	$('#pageContent').css('display','block');
-	checkURL(hashstr); 
-	}
-	});
-});
+
 </script>		
 @endsection
